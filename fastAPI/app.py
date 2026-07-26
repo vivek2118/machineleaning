68332dd6af276d8a8e4d0babff6b3b57722e3774
vivek mane <vivmane2111@gmail.com>
2026-07-26 23:14:09 +0530

    find by id and sorting tech learn in project

--- fastAPI/app.py
+++ fastAPI/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 
 app=FastAPI()
@@ -22,3 +22,24 @@ def view():
     data=load_data()
     return data
 
+@app.get('/patients/{patients_id}')
+def patients(patients_id:str = Path(...,description="id of the patients in the DB",example="P001")):
+    data=load_data()
+    if patients_id in data:
+        return data[patients_id]
+    raise HTTPException(status_code=404,detail='patient not found in data base')
+
+@app.get('/sort')
+def sort_patients(sort_by:str = Query(...,description='sort in the basis of height, weight or bmi'), order:str = Query('acending',description='sort acending ort decending order')):
+    valid_fields=['weight','height','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'invalid fields, select from{valid_fields}')
+    if order not in ['acending','decending']:
+        raise HTTPException(status_code=400,detail='invalide order select between acending or decending')
+    data=load_data()
+
+    sort_order=True if order=='decending' else False
+
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+
+    return sorted_data
